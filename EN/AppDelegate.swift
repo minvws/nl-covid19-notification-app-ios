90822fc88f85fbecc69280c25fb1b30a6dabb97c
Rob Mulder <r.mulder5@dictu.nl>
2020-06-02 11:16:27 +0200

    Initial setup

---
+++ EN/AppDelegate.swift
@@ -0,0 +1,30 @@
+/*
+* Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+
+        guard let window = window else {
+            fatalError("AppDelegate - Window not found!")
+        }
+        
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
