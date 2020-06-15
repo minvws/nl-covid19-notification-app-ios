99c73b0a39b679800aa2fe523c2cdcfbc899557f
Robin van Dijke <robinvandijke@gmail.com>
2020-06-15 09:52:54 +0200

    Update Header

--- EN/App/Features/Main/Status/StatusCardView.swift
+++ EN/App/Features/Main/Status/StatusCardView.swift
@@ -1,9 +1,9 @@
-//
-//  StatusCardView.swift
-//  EN
-//
-//  Created by Thomas Visser on 14/06/2020.
-//
+/*
+* Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
 
 import Foundation
 import UIKit
