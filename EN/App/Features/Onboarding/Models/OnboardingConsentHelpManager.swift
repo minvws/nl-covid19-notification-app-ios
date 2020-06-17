496fb8d42a6328527da85f6864a68117a7c835eb
Rob Mulder <hallo@robmulder.com>
2020-06-17 16:21:25 +0200

    Fixed headers

--- EN/App/Features/Onboarding/Models/OnboardingConsentHelpManager.swift
+++ EN/App/Features/Onboarding/Models/OnboardingConsentHelpManager.swift
@@ -1,9 +1,10 @@
-//
-//  OnboardingConsentHelpManager.swift
-//  EN
-//
-//  Created by Rob Mulder on 17/06/2020.
-//
+/*
+* Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+*
+*  SPDX-License-Identifier: EUPL-1.2
+*/
+
 
 import Combine
 import UIKit
