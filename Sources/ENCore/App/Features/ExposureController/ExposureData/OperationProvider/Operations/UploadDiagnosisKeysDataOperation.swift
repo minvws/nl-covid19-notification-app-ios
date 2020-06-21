c398c9915f01a6b7241f6ee1617200c96e4c04cc
Robin van Dijke <robinvandijke@gmail.com>
2020-06-21 14:48:29 +0200

    Fix incorrect file rename

--- Sources/ENCore/App/Features/ExposureController/ExposureData/OperationProvider/Operations/UploadDiagnosisKeysDataOperation.swift
+++ Sources/ENCore/App/Features/ExposureController/ExposureData/OperationProvider/Operations/UploadDiagnosisKeysDataOperation.swift
@@ -5,4 +5,26 @@
  *  SPDX-License-Identifier: EUPL-1.2
  */
 
+import Combine
 import Foundation
+
+final class UploadDiagnosisKeysDataOperation: ExposureDataOperation {
+    init(networkController: NetworkControlling,
+         diagnosisKeys: [DiagnosisKey],
+         labConfirmationKey: LabConfirmationKey) {
+        self.networkController = networkController
+        self.diagnosisKeys = diagnosisKeys
+        self.labConfirmationKey = labConfirmationKey
+    }
+
+    func execute() -> AnyPublisher<(), ExposureDataError> {
+        return networkController
+            .postKeys(keys: diagnosisKeys, labConfirmationKey: labConfirmationKey)
+            .mapError { (error: NetworkError) -> ExposureDataError in error.asExposureDataError }
+            .eraseToAnyPublisher()
+    }
+
+    private let networkController: NetworkControlling
+    private let diagnosisKeys: [DiagnosisKey]
+    private let labConfirmationKey: LabConfirmationKey
+}
