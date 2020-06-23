93d535545b4fc5830ace3201b79f0f5c58cb0de2
Cameron Mc Gorian <cameron@sbarow.com>
2020-06-23 15:06:07 +0200

    Localize MessageView

--- Sources/ENCore/App/Features/Message/MessageViewController.swift
+++ Sources/ENCore/App/Features/Message/MessageViewController.swift
@@ -38,7 +38,7 @@ final class MessageViewController: ViewController, MessageViewControllable, UIAd
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = "Melding gekregen"
+        title = Localization.string(for: "message.title")
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close,
                                                             target: self,
                                                             action: #selector(didTapCloseButton(sender:)))
@@ -84,7 +84,8 @@ private final class MessageView: View {
     init(theme: Theme, title: String, body: String) {
         self.title = title
         self.body = body
-        let config = InfoViewConfig(actionButtonTitle: "Bel voor coronatest", headerImage: Image.named("MessageHeader"))
+        let config = InfoViewConfig(actionButtonTitle: Localization.string(for: "message.button.title"),
+                                    headerImage: Image.named("MessageHeader"))
         self.infoView = InfoView(theme: theme, config: config)
         super.init(theme: theme)
     }
@@ -119,24 +120,31 @@ private final class MessageView: View {
     }
 
     private func complaints() -> View {
-        let list = NSAttributedString.bulletList(["(licht) hoesten", "loopneus, niezen, keelpijn", "verlies van geur en/of smaak kortademigheid/benauwdheid", "koorts boven de 38 graden"],
-                                                 theme: theme,
-                                                 font: theme.fonts.body)
-        let content = NSAttributedString(string: "Heb je klachten? Blijf dan zoveel mogelijk thuis. Zijn het ernstige klachten? Bel meteen je huisarts.")
+        let list = [
+            Localization.string(for: "moreInformation.complaints.item1"),
+            Localization.string(for: "moreInformation.complaints.item2"),
+            Localization.string(for: "moreInformation.complaints.item3"),
+            Localization.string(for: "moreInformation.complaints.item4")
+        ]
+        let bulletList = NSAttributedString.bulletList(list, theme: theme, font: theme.fonts.body)
+        let content = Localization.attributedString(for: "moreInformation.complaints.content")
 
         let string = NSMutableAttributedString()
-        string.append(list)
+        string.append(bulletList)
         string.append(content)
-        return InfoSectionTextView(theme: theme, title: "Klachten die passen bij het coronavirus (COVID-19)", content: string)
+        return InfoSectionTextView(theme: theme,
+                                   title: Localization.string(for: "moreInformatio.complaints.title"),
+                                   content: string)
     }
 
     private func doCoronaTest() -> View {
-        // TODO: Bold Phone Number
-        InfoSectionTextView(theme: theme, title: "Doe een coronatest", content: NSAttributedString(string: "De GGD raadt aan je te laten testen op het virus, zelfs als je je nog niet ziek voelt. Want zonder je ziek te voelen kun je het virus al verspreiden en zo anderen besmetten.\n\nTesten is gratis en kan meestal snel gebeuren. Blijf zo veel mogelijk thuis tot de uitslag bekend is.\n\nBel gratis 0800-1202 om een coranatest aan te vragen."))
+        InfoSectionTextView(theme: theme,
+                            title: Localization.string(for: "moreInformation.receivedNotification.doCoronaTest.title"),
+                            content: Localization.attributedString(for: "moreInformation.receivedNotification.doCoronaTest.content"))
     }
 
     private func info() -> View {
-        let string = NSAttributedString(string: "Houd je burgerservicenummer bij de hand. Dit vind je op je paspoort of identiteitskaart.")
+        let string = Localization.attributedString(for: "moreInformation.info.title")
         return InfoSectionCalloutView(theme: theme, content: string)
     }
 }
