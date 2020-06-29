15f39d4fcc32f0c3a456b9ef4502e40e6621bf10
Robin van Dijke <robinvandijke@gmail.com>
2020-06-29 21:52:43 +0200

    Fix Developer Menu Build Setting in AppCenter

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+if [ -z "$USE_DEVELOPER_MENU" ]
+then
+      USE_DEVELOPER_MENU=false
+fi
+
+
+if [ -z "$NETWORK_CONFIGURATION" ]
+then
+      NETWORK_CONFIGURATION="Production"
+fi
+
+brew install yq
+yq w -i project.yml "targets.ENCore.settings.base.USE_DEVELOPER_MENU" ${USE_DEVELOPER_MENU}
+yq w -i project.yml "targets.ENCore.settings.base.NETWORK_CONFIGURATION" ${NETWORK_CONFIGURATION}
+
 make install_dev_deps && make project
