2fa1882d9a4f46349777aa4f11023665e56a11b3
Robin van Dijke <robinvandijke@gmail.com>
2020-06-30 20:07:24 +0200

    More logging

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -20,4 +20,6 @@ brew install yq
 yq w -i project.yml "targets.ENCore.settings.base.USE_DEVELOPER_MENU" ${USE_DEVELOPER_MENU}
 yq w -i project.yml "targets.EN.settings.base.NETWORK_CONFIGURATION" ${NETWORK_CONFIGURATION}
 
+cat project.yml
+
 make install_dev_deps && make generate_project
