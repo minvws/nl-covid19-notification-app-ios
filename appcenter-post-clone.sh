abd38cf6777e18ca25b549ef91a38e1f11892a4e
Robin van Dijke <robinvandijke@gmail.com>
2020-06-30 22:15:32 +0200

    Learn how to properly use swift compiler flags :sigh:

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -14,8 +14,12 @@ then
 fi
 
 brew install yq
-yq w -i project.yml "targets.ENCore.settings.base.USE_DEVELOPER_MENU" ${USE_DEVELOPER_MENU}
 yq w -i project.yml "targets.EN.settings.base.NETWORK_CONFIGURATION" ${NETWORK_CONFIGURATION}
+
+if [ -n "$USE_DEVELOPER_MENU" ]
+then
+	yq w -i project.yml -- "targets.ENCore.settings.base.OTHER_SWIFT_FLAGS" -DUSE_DEVELOPER_MENU
+fi
 
 cat project.yml
 
