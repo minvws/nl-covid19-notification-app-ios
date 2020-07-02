5e57a89d6ee287bb085b814dfb35eafd3a56e57c
Leon Boon <l.boon@dictu.nl>
2020-07-02 20:50:47 +0200

    script change

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,10 +3,12 @@
 touch Cartfile
 env
 
-if [ -z "$USE_DEVELOPER_MENU" ]
+if [ ! -z "$USE_DEVELOPER_MENU" ]
 then
-      USE_DEVELOPER_MENU=true
+	echo "TEST => USE DEV MENU!!!!!"
+      #USE_DEVELOPER_MENU=true
 fi
+exit
 
 
 if [ -z "$NETWORK_CONFIGURATION" ]
@@ -22,11 +24,12 @@ fi
 brew install yq
 yq w -i project.yml "targets.EN.settings.base.NETWORK_CONFIGURATION" ${NETWORK_CONFIGURATION}
 yq w -i project.yml "targets.EN.settings.base.LOG_LEVEL" ${LOG_LEVEL}
+yq w -i project.yml "targets.EN.settings.base.USE_DEVELOPER_MENU" ${USE_DEVELOPER_MENU}
 
-if [ -n "$USE_DEVELOPER_MENU" ]
-then
-	yq w -i project.yml -- "targets.ENCore.settings.base.OTHER_SWIFT_FLAGS" -DUSE_DEVELOPER_MENU
-fi
+# if [ -n "$USE_DEVELOPER_MENU" ]
+# then
+# 	yq w -i project.yml -- "targets.ENCore.settings.base.OTHER_SWIFT_FLAGS" -DUSE_DEVELOPER_MENU
+# fi
 
 cat project.yml
 
