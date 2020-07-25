aeee5d357865efa4f53bdc5d4b5ffa9f12bb0468
Diego Marcon <dm.marcon@gmail.com>
2020-07-25 15:18:23 +0200

    Scaffold tableview and sections on AboutViewController

--- Sources/ENCore/App/Features/About/AboutViewController.swift
+++ Sources/ENCore/App/Features/About/AboutViewController.swift
@@ -13,7 +13,7 @@ import WebKit
 /// @mockable
 protocol AboutRouting: Routing {}
 
-final class AboutViewController: ViewController, AboutViewControllable {
+final class AboutViewController: ViewController, AboutViewControllable, UIAdaptivePresentationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     weak var router: AboutRouting?
 
     init(listener: AboutListener, theme: Theme) {
@@ -36,6 +36,9 @@ final class AboutViewController: ViewController, AboutViewControllable {
         super.viewDidLoad()
         modalPresentationStyle = .popover
 
+        internalView.tableView.dataSource = self
+        internalView.tableView.delegate = self
+
         navigationItem.rightBarButtonItem = closeBarButtonItem
     }
 
@@ -43,13 +46,161 @@ final class AboutViewController: ViewController, AboutViewControllable {
         listener?.aboutRequestsDismissal(shouldHideViewController: true)
     }
 
+    // MARK: - TableView
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        let cellIdentifier = "AboutCell"
+
+        if let aCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+            cell = aCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+
+        cell.textLabel?.text = "An example cell"
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.font = theme.fonts.body
+        cell.textLabel?.accessibilityTraits = .header
+
+        cell.accessoryType = .disclosureIndicator
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        return firstSectionHeader
+    }
+
     // MARK: - Private
 
     private weak var listener: AboutListener?
     private lazy var internalView: AboutView = AboutView(theme: self.theme)
+
     private lazy var closeBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close,
                                                           target: self,
                                                           action: #selector(didTapClose))
+
+    private lazy var firstSectionHeader: SectionHeaderView = {
+        let headerView = SectionHeaderView(theme: theme)
+        headerView.sectionHeaderLabel.text = "First section Header"
+        headerView.titleLabel.text = String.moreInformationAboutTitle
+
+        let button = CardButton(title: "This is a test title", subtitle: "And a test subtile to check layout", image: UIImage.githubLogo, theme: theme)
+        button.backgroundColor = theme.colors.inactive
+        headerView.addSections([button])
+
+        return headerView
+    }()
 }
 
-private final class AboutView: View {}
+private final class AboutView: View {
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        tableView.separatorStyle = .singleLine
+        tableView.backgroundColor = .clear
+
+        tableView.showsVerticalScrollIndicator = true
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.isScrollEnabled = true
+
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableView.automaticDimension
+
+        tableView.estimatedSectionHeaderHeight = 300
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+
+        tableView.allowsMultipleSelection = false
+        tableView.tableFooterView = UIView()
+
+        return tableView
+    }()
+
+    override func build() {
+        super.build()
+
+        addSubview(tableView)
+    }
+
+    override func setupConstraints() {
+        super.setupConstraints()
+        tableView.snp.makeConstraints { maker in
+            maker.leading.trailing.top.bottom.equalToSuperview()
+        }
+    }
+}
+
+private final class SectionHeaderView: View {
+
+    let titleLabel = Label(frame: .zero)
+    let sectionHeaderLabel = Label(frame: .zero)
+
+    override func build() {
+        super.build()
+
+        titleLabel.numberOfLines = 0
+        titleLabel.accessibilityTraits = .header
+        titleLabel.font = theme.fonts.largeTitle
+        titleLabel.accessibilityTraits = .header
+
+        sectionHeaderLabel.numberOfLines = 0
+        sectionHeaderLabel.accessibilityTraits = .header
+        sectionHeaderLabel.font = theme.fonts.subheadBold
+        sectionHeaderLabel.textColor = theme.colors.primary
+
+        addSubview(titleLabel)
+        addSubview(stackView)
+        addSubview(sectionHeaderLabel)
+    }
+
+    override func setupConstraints() {
+        super.setupConstraints()
+
+        hasBottomMargin = true
+
+        titleLabel.snp.makeConstraints { maker in
+            maker.leading.trailing.top.equalToSuperview().inset(16)
+        }
+
+        stackView.snp.makeConstraints { maker in
+            maker.top.equalTo(titleLabel.snp.bottom).offset(16)
+            maker.leading.trailing.equalToSuperview().inset(16)
+        }
+
+        sectionHeaderLabel.snp.makeConstraints { maker in
+            maker.top.equalTo(stackView.snp.bottom).offset(16)
+            maker.leading.trailing.bottom.equalToSuperview().inset(16)
+        }
+    }
+
+    func addSections(_ views: [UIView]) {
+        for view in views {
+            stackView.addArrangedSubview(view)
+        }
+    }
+
+    // MARK: - Private
+
+    private let stackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 16
+        stackView.backgroundColor = .clear
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+}
