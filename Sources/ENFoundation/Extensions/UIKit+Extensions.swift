8f11232bbb09c6abbaa38eeefd18e2f2ae98ad3d
Diego Marcon <dm.marcon@gmail.com>
2020-08-12 11:16:41 +0200

    Move extension to ENFoundation

--- Sources/ENFoundation/Extensions/UIKit+Extensions.swift
+++ Sources/ENFoundation/Extensions/UIKit+Extensions.swift
@@ -21,3 +21,16 @@ public extension NSLayoutConstraint {
         return self
     }
 }
+
+public extension UIDevice {
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
+    }()
+}
