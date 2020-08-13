0325e13eb5e2b274099a0594edf910f56eca0f90
Rob Mulder <hallo@robmulder.com>
2020-08-13 11:07:37 +0200

    Added markup support for the app information page

--- Sources/ENCore/App/Features/About/AppInformation/AppInformationViewController.swift
+++ Sources/ENCore/App/Features/About/AppInformation/AppInformationViewController.swift
@@ -64,28 +64,28 @@ private final class AppInformationView: View {
 
     private lazy var protectView = InformationCardView(theme: theme,
                                                        image: UIImage.appInformationProtect,
-                                                       title: String.helpWhatAppDoesProtectTitle.attributed(),
+                                                       title: NSAttributedString.makeFromHtml(text: String.helpWhatAppDoesProtectTitle, font: theme.fonts.title2, textColor: .black),
                                                        message: String.helpWhatAppDoesProtectDescription.attributed())
 
     private lazy var notifyView = InformationCardView(theme: theme,
                                                       image: UIImage.appInformationNotify,
-                                                      title: String.helpWhatAppDoesNotifyTitle.attributed(),
+                                                      title: NSAttributedString.makeFromHtml(text: String.helpWhatAppDoesNotifyTitle, font: theme.fonts.title2, textColor: .black),
                                                       message: String.helpWhatAppDoesNotifyDescription.attributed())
 
     private lazy var bluetoothView = InformationCardView(theme: theme,
                                                          image: UIImage.appInformationBluetooth,
-                                                         title: String.helpWhatAppDoesBluetoothTitle.attributed(),
+                                                         title: NSAttributedString.makeFromHtml(text: String.helpWhatAppDoesBluetoothTitle, font: theme.fonts.title2, textColor: .black),
                                                          message: String.helpWhatAppDoesBluetoothDescription.attributed())
 
     private lazy var cycleExampleView = InformationCardView(theme: theme,
                                                             image: UIImage.appInformationExampleCycle,
                                                             pretitle: String.example.attributed(),
-                                                            title: String.helpWhatAppDoesExampleCycleTitle.attributed(),
+                                                            title: NSAttributedString.makeFromHtml(text: String.helpWhatAppDoesExampleCycleTitle, font: theme.fonts.title2, textColor: .black),
                                                             message: String.helpWhatAppDoesExampleCycleDescription.attributed())
 
     private lazy var trainExampleView = InformationCardView(theme: theme,
                                                             image: UIImage.appInformationExampleTrain,
                                                             pretitle: String.example.attributed(),
-                                                            title: String.helpWhatAppDoesExampleTrainTitle.attributed(),
+                                                            title: NSAttributedString.makeFromHtml(text: String.helpWhatAppDoesExampleTrainTitle, font: theme.fonts.title2, textColor: .black),
                                                             message: String.helpWhatAppDoesExampleTrainDescription.attributed())
 }
