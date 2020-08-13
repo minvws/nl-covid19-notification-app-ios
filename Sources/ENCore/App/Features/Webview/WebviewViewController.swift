2ca3b9438a5637ac689fdbf824ed85129f98486b
Diego Marcon <dm.marcon@gmail.com>
2020-08-13 16:38:14 +0200

    Extract a simpe WebviewViewController

---
+++ Sources/ENCore/App/Features/Webview/WebviewViewController.swift
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import ENFoundation
+import UIKit
+import WebKit
+
+final class WebviewViewController: ViewController, Logging {
+
+    init(listener: WebviewListener, url: URL, theme: Theme) {
+        self.listener = listener
+        self.url = url
+
+        super.init(theme: theme)
+    }
+
+    // MARK: - ViewController Lifecycle
+
+    override func loadView() {
+        self.view = internalView
+        self.view.frame = UIScreen.main.bounds
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if webViewLoadingEnabled() {
+            internalView.webView.load(URLRequest(url: url))
+        } else {
+            logDebug("`webViewLoading` disabled")
+        }
+
+        navigationItem.rightBarButtonItem = navigationController?.navigationItem.rightBarButtonItem
+    }
+
+    // MARK: - Private
+
+    private weak var listener: WebviewListener?
+    private let url: URL
+    private let internalView = WebviewView()
+}
+
+private final class WebviewView: View {
+
+    lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.allowsBackForwardNavigationGestures = false
+        return webView
+    }()
+
+    private lazy var gradientImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .clear
+        imageView.image = .gradient ?? UIImage()
+        return imageView
+    }()
+
+    override func build() {
+        super.build()
+        addSubview(webView)
+        addSubview(gradientImageView)
+    }
+
+    override func setupConstraints() {
+        super.setupConstraints()
+
+        webView.snp.makeConstraints { maker in
+            maker.edges.equalToSuperview()
+        }
+
+        gradientImageView.snp.makeConstraints { maker in
+            maker.leading.trailing.bottom.equalToSuperview()
+            maker.height.equalTo(25)
+        }
+    }
+}
