f70a29b9bcf4a00021d0d59cc5aae175e851bb58
Diego Marcon <dm.marcon@gmail.com>
2020-08-29 15:52:54 +0200

    Update tests

--- Sources/ENCoreTests/App/Features/About/AboutRouterTests.swift
+++ Sources/ENCoreTests/App/Features/About/AboutRouterTests.swift
@@ -76,7 +76,7 @@ final class AboutRouterTests: TestCase {
         XCTAssertEqual(helpDetailBuilder.buildCallCount, 0)
         XCTAssertEqual(viewController.pushCallCount, 0)
 
-        router.routeToAboutEntry(entry: .question(title: "question", answer: "answer"))
+        router.routeToAboutEntry(entry: .question(HelpQuestion(question: "question", answer: "answer")))
 
         XCTAssertEqual(helpDetailBuilder.buildCallCount, 1)
         XCTAssertEqual(viewController.pushCallCount, 1)
