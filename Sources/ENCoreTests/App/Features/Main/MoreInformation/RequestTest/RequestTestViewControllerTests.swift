117de8d8da52a88d4a0af56a1c555f89c98a42bb
Cameron Mc Gorian <cameron@sbarow.com>
2020-09-01 16:04:49 +0200

    Fix test

--- Sources/ENCoreTests/App/Features/Main/MoreInformation/RequestTest/RequestTestViewControllerTests.swift
+++ Sources/ENCoreTests/App/Features/Main/MoreInformation/RequestTest/RequestTestViewControllerTests.swift
@@ -17,7 +17,7 @@ final class RequestTestViewControllerTests: TestCase {
     override func setUp() {
         super.setUp()
 
-        recordSnapshots = true
+        recordSnapshots = false
 
         viewController = RequestTestViewController(listener: listern, theme: theme)
     }
