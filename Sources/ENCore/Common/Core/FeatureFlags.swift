6259f0d793afcc78748a52eb97aa97dcf4f41c2f
Roel Spruit <roelspruit@gmail.com>
2020-10-12 11:58:05 +0200

    Added a basic feature flag setup to prevent non-finished screens from being released

---
+++ Sources/ENCore/Common/Core/FeatureFlags.swift
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import Foundation
+
+class FeatureFlags {
+
+    struct FeatureFlag {
+        let description: String
+        let enabled: Bool
+    }
+
+    static var exposureNotificationExplanation = FeatureFlag(description: "iOS 13.7+ EN setting instructions screen", enabled: false)
+}
