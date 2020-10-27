b90827075cb98c65c2523c23cebd48bbb931338e
Roel Spruit <roelspruit@gmail.com>
2020-10-27 16:01:27 +0100

    Explanatory comment

--- Sources/ENCore/Common/Streams/InterfaceOrientationStream.swift
+++ Sources/ENCore/Common/Streams/InterfaceOrientationStream.swift
@@ -24,6 +24,8 @@ final class InterfaceOrientationStream: InterfaceOrientationStreaming {
 
         currentOrientationIsLandscape = windowScene.interfaceOrientation.isLandscape
 
+        // We listen for device orientation changes (which are more sensitive)
+        // but we use the interface orientation of the key window to actually determine the rotation of the UI
         NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification, object: nil, queue: .main) { [weak self] _ in
             self?.currentOrientationIsLandscape = windowScene.interfaceOrientation.isLandscape
             self?.subject.send(windowScene.interfaceOrientation.isLandscape)
