7ac44576d7b7b43d6ff0a6125485548b3ae9e4a5
Roel Spruit <roelspruit@gmail.com>
2020-11-02 16:00:38 +0100

    Updates snapshot tests with mock data and bulletpoints

--- Sources/ENCoreTests/App/Features/Notifications/Message/MessageViewControllerTests.swift
+++ Sources/ENCoreTests/App/Features/Notifications/Message/MessageViewControllerTests.swift
@@ -14,24 +14,28 @@ import XCTest
 final class MessageViewControllerTests: TestCase {
 
     private var viewController: MessageViewController!
-    private var listner: MessageListenerMock!
+    private var listener: MessageListenerMock!
     private var storageController: StorageControllingMock!
-    private var messageManager: MessageManager!
+    private var messageManager: MessageManagingMock!
     private var exposureDate: Date!
 
     override func setUp() {
         super.setUp()
 
-        listner = MessageListenerMock()
+        listener = MessageListenerMock()
         storageController = StorageControllingMock()
-        messageManager = MessageManager(storageController: storageController, theme: self.theme)
+        messageManager = MessageManagingMock()
+
+        messageManager.getLocalizedTreatmentPerspectiveHandler = { date in
+            self.fakeMessage
+        }
 
         recordSnapshots = false
 
         DateTimeTestingOverrides.overriddenCurrentDate = Date(timeIntervalSince1970: 1593538088) // 30/06/20 17:28
         exposureDate = Date(timeIntervalSince1970: 1593290000) // 27/06/20 20:33
 
-        viewController = MessageViewController(listener: listner,
+        viewController = MessageViewController(listener: listener,
                                                theme: theme,
                                                exposureDate: exposureDate,
                                                messageManager: messageManager)
@@ -44,12 +48,22 @@ final class MessageViewControllerTests: TestCase {
     }
 
     func testPresentationControllerDidDismissCallsListener() {
-        listner.messageWantsDismissalHandler = { value in
+        listener.messageWantsDismissalHandler = { value in
             XCTAssertFalse(value)
         }
 
         viewController.presentationControllerDidDismiss(UIPresentationController(presentedViewController: viewController, presenting: nil))
 
-        XCTAssertEqual(listner.messageWantsDismissalCallCount, 1)
+        XCTAssertEqual(listener.messageWantsDismissalCallCount, 1)
     }
+
+    // MARK: - Private
+
+    private lazy var fakeMessage: LocalizedTreatmentPerspective = {
+        LocalizedTreatmentPerspective(paragraphs: [
+            .init(title: NSAttributedString(string: "Paragraph Title"),
+                  body: .htmlWithBulletList(text: "<ul><li>List Item 1</li><li>List Item 2</li></ul>Some Other Paragraph", font: self.theme.fonts.body, textColor: self.theme.colors.gray, theme: self.theme),
+                  type: .paragraph)
+        ], quarantineDays: 10)
+    }()
 }
