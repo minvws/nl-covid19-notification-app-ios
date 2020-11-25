b2e3b170a2489192ddebbfee77a745223bd0bca9
Roel Spruit <roelspruit@gmail.com>
2020-11-25 15:56:36 +0100

    Center webview error and take safe area into account

--- Sources/ENCoreTests/App/Features/WebView/WebViewErrorViewSnapshotTests.swift
+++ Sources/ENCoreTests/App/Features/WebView/WebViewErrorViewSnapshotTests.swift
@@ -14,6 +14,7 @@ class WebViewErrorViewSnapshotTests: TestCase {
     private var sut: WebViewErrorView!
 
     override func setUpWithError() throws {
+        recordSnapshots = true
         sut = WebViewErrorView(theme: theme)
     }
 
