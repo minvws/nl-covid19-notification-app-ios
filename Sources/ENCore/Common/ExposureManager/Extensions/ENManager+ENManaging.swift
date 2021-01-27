3ce4059d782279f4aa8f5cdc485bed90b503469a
Roel Spruit <roelspruit@gmail.com>
2021-01-27 14:15:32 +0100

    Move to v2 api

--- Sources/ENCore/Common/ExposureManager/Extensions/ENManager+ENManaging.swift
+++ Sources/ENCore/Common/ExposureManager/Extensions/ENManager+ENManaging.swift
@@ -53,6 +53,40 @@ private class DefaultExposureConfiguration: ENExposureConfiguration {
         self.durationLevelValues = exposureConfiguration.durationLevelValues as [NSNumber]
         self.transmissionRiskLevelValues = exposureConfiguration.transmissionRiskLevelValues as [NSNumber]
         self.metadata = ["attenuationDurationThresholds": exposureConfiguration.attenuationDurationThresholds]
+
+        // Needed since EN API v2 version
+        self.reportTypeNoneMap = .confirmedTest
+        self.infectiousnessForDaysSinceOnsetOfSymptoms = [
+            -14: NSNumber(value: ENInfectiousness.high.rawValue),
+            -13: NSNumber(value: ENInfectiousness.high.rawValue),
+            -12: NSNumber(value: ENInfectiousness.high.rawValue),
+            -11: NSNumber(value: ENInfectiousness.high.rawValue),
+            -10: NSNumber(value: ENInfectiousness.high.rawValue),
+            -9: NSNumber(value: ENInfectiousness.high.rawValue),
+            -8: NSNumber(value: ENInfectiousness.high.rawValue),
+            -7: NSNumber(value: ENInfectiousness.high.rawValue),
+            -6: NSNumber(value: ENInfectiousness.high.rawValue),
+            -5: NSNumber(value: ENInfectiousness.high.rawValue),
+            -4: NSNumber(value: ENInfectiousness.high.rawValue),
+            -3: NSNumber(value: ENInfectiousness.high.rawValue),
+            -2: NSNumber(value: ENInfectiousness.high.rawValue),
+            -1: NSNumber(value: ENInfectiousness.high.rawValue),
+            0: NSNumber(value: ENInfectiousness.high.rawValue),
+            1: NSNumber(value: ENInfectiousness.high.rawValue),
+            2: NSNumber(value: ENInfectiousness.high.rawValue),
+            3: NSNumber(value: ENInfectiousness.high.rawValue),
+            4: NSNumber(value: ENInfectiousness.high.rawValue),
+            5: NSNumber(value: ENInfectiousness.high.rawValue),
+            6: NSNumber(value: ENInfectiousness.high.rawValue),
+            7: NSNumber(value: ENInfectiousness.high.rawValue),
+            8: NSNumber(value: ENInfectiousness.high.rawValue),
+            9: NSNumber(value: ENInfectiousness.high.rawValue),
+            10: NSNumber(value: ENInfectiousness.high.rawValue),
+            11: NSNumber(value: ENInfectiousness.high.rawValue),
+            12: NSNumber(value: ENInfectiousness.high.rawValue),
+            13: NSNumber(value: ENInfectiousness.high.rawValue),
+            14: NSNumber(value: ENInfectiousness.high.rawValue)
+        ]
     }
 
     // MARK: - Private
