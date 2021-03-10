1efe0468952d0f086cbc46246f85b029a2401b15
Roel Spruit <roelspruit@gmail.com>
2021-03-10 16:41:37 +0100

    remove my name from header

--- Sources/ENCore/Common/Alert/AlertControllerBuilder.swift
+++ Sources/ENCore/Common/Alert/AlertControllerBuilder.swift
@@ -1,9 +1,9 @@
-//
-//  AlertControllerBuilder.swift
-//  ENCore
-//
-//  Created by Roel Spruit on 10/03/2021.
-//
+/*
+ * Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
 
 import Foundation
 import UIKit
