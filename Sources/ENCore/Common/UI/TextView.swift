9bfbd7f40b62f8042c3c1f95afd7d5003df9f774
Roel Spruit <roelspruit@gmail.com>
2021-04-12 08:59:05 +0200

    Cleanup

--- Sources/ENCore/Common/UI/TextView.swift
+++ Sources/ENCore/Common/UI/TextView.swift
@@ -25,37 +25,10 @@ class TextView: UITextView {
         set {
             super.attributedText = newValue
             
-            guard let newText = newValue, newText.length > 0 else {
-                return
-            }
-
-            let attributes = newText.attributes(at: 0, effectiveRange: nil)
-
-            guard let accessibilityTextCustom = attributes.filter({ (attribute) -> Bool in
-                attribute.key == .accessibilityTextCustom
-            }).first else {
-                return
-            }
-
-            guard let data = accessibilityTextCustom.value as? [String: Int] else {
-                return
-            }
-
-            guard let index = data[NSAttributedString.AccessibilityTextCustomValue.accessibilityListIndex.rawValue],
-                let total = data[NSAttributedString.AccessibilityTextCustomValue.accessibilityListSize.rawValue] else {
-                return
-            }
-
-            if total > 1 {
-                if index == 0 {
-                    accessibilityHint = .accessibilityStartOfList
-                } else if index == total - 1 {
-                    accessibilityHint = .accessibilityEndOfList
-                }
-            }
+            updateListAccessibilityHint()
         }
     }
-
+    
     // Point detection on this textview is overridden to only allow touch on links, not on normal text
     override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
         return linkExists(atPoint: point)
@@ -73,5 +46,34 @@ class TextView: UITextView {
 
         let startIndex = offset(from: beginningOfDocument, to: range.start)
         return attributedText.attribute(.link, at: startIndex, effectiveRange: nil) != nil
+    }
+    
+    /// Adds a start of list / end of list accessibility hint to this TextView if the attributedtext contains a  custom accessibility text attributes
+    private func updateListAccessibilityHint() {
+        guard let attributedText = self.attributedText, attributedText.length > 0 else {
+            return
+        }
+        
+        let attributes = attributedText.attributes(at: 0, effectiveRange: nil)
+
+        guard let accessibilityTextCustom = attributes.filter({ (attribute) -> Bool in
+            attribute.key == .accessibilityTextCustom
+        }).first else {
+            return
+        }
+
+        guard let data = accessibilityTextCustom.value as? [String: Int],
+              let index = data[NSAttributedString.AccessibilityTextCustomValue.accessibilityListIndex.rawValue],
+              let total = data[NSAttributedString.AccessibilityTextCustomValue.accessibilityListSize.rawValue] else {
+            return
+        }
+
+        if total > 1 {
+            if index == 0 {
+                accessibilityHint = .accessibilityStartOfList
+            } else if index == total - 1 {
+                accessibilityHint = .accessibilityEndOfList
+            }
+        }
     }
 }
