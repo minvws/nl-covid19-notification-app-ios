1d75c778620ba21385452a4dda05d91b074ef7a6
Roel Spruit <roelspruit@gmail.com>
2021-06-11 11:46:21 +0200

    Updated tests

--- Sources/ENCoreTests/App/Features/Main/MoreInformation/KeySharing/ThankYou/ThankYouViewControllerTests.swift
+++ Sources/ENCoreTests/App/Features/Main/MoreInformation/KeySharing/ThankYou/ThankYouViewControllerTests.swift
@@ -13,33 +13,61 @@ import SnapshotTesting
 import XCTest
 
 final class ThankYouViewControllerTests: TestCase {
-    private var viewController: ThankYouViewController!
-    private let listener = ThankYouListenerMock()
-    private let interfaceOrientationStream = InterfaceOrientationStreamingMock()
+    private var sut: ThankYouViewController!
+    
+    private var mockListener: ThankYouListenerMock!
+    private var mockInterfaceOrientationStream: InterfaceOrientationStreamingMock!
+    private var mockFeatureFlagController: FeatureFlagControllingMock!
+    private var labConfirmationKey: LabConfirmationKey!
 
     // MARK: - Setup
-
+    
     override func setUp() {
         super.setUp()
-
+        
         recordSnapshots = false
-
-        interfaceOrientationStream.isLandscape = BehaviorSubject(value: false)
-
-        let key = LabConfirmationKey(identifier: "ABCDEFG".asGGDkey,
+        
+        mockListener = ThankYouListenerMock()
+        mockInterfaceOrientationStream = InterfaceOrientationStreamingMock()
+        mockFeatureFlagController = FeatureFlagControllingMock()
+        
+        mockInterfaceOrientationStream.isLandscape = BehaviorSubject(value: false)
+        
+        labConfirmationKey = LabConfirmationKey(identifier: "ABCDEFG".asGGDkey,
                                      bucketIdentifier: Data(),
                                      confirmationKey: Data(),
                                      validUntil: currentDate())
-
-        viewController = ThankYouViewController(listener: listener,
-                                                theme: theme,
-                                                exposureConfirmationKey: key,
-                                                interfaceOrientationStream: interfaceOrientationStream)
     }
-
+    
     // MARK: - Tests
-
+    
     func test_thankYou_snapshotStateLoading() {
-        snapshots(matching: viewController)
+        
+        mockFeatureFlagController.isFeatureFlagEnabledHandler = { feature in
+            return feature != .independentKeySharing
+        }
+        
+        sut = ThankYouViewController(listener: mockListener,
+                                     theme: theme,
+                                     exposureConfirmationKey: labConfirmationKey,
+                                     interfaceOrientationStream: mockInterfaceOrientationStream,
+                                     featureFlagController: mockFeatureFlagController)
+        
+        snapshots(matching: sut)
+    }
+    
+    func test_thankYou_snapshotStateLoading_withIndependentKeySharing() {
+        
+        mockFeatureFlagController.isFeatureFlagEnabledHandler = { feature in
+            return feature == .independentKeySharing
+        }
+        
+        sut = ThankYouViewController(listener: mockListener,
+                                     theme: theme,
+                                     exposureConfirmationKey: labConfirmationKey,
+                                     interfaceOrientationStream: mockInterfaceOrientationStream,
+                                     featureFlagController: mockFeatureFlagController)
+        
+        snapshots(matching: sut)
     }
 }
