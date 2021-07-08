e6d7c49abfe2f31b43daba901a97dbb3f2b2611d
Rob Mulder <hallo@robmulder.com>
2021-07-08 12:19:12 +0200

    Added RootViewController test coverage

--- Sources/ENCoreTests/App/Features/Root/RootViewControllerTests.swift
+++ Sources/ENCoreTests/App/Features/Root/RootViewControllerTests.swift
@@ -10,14 +10,65 @@ import Foundation
 import XCTest
 
 final class RootViewControllerTests: TestCase {
+    private var mock: RootViewControllableMock!
     private var viewController: RootViewController!
+    private var emptyViewController: ViewController!
     private let router = RootRoutingMock()
 
     override func setUp() {
         super.setUp()
 
+        recordSnapshots = true
         viewController = RootViewController(theme: theme)
         viewController.router = router
+
+        emptyViewController = ViewController(theme: theme)
+        emptyViewController.view.backgroundColor = theme.colors.ok
+
+        mock = RootViewControllableMock(uiviewController: viewController, router: router)
+    }
+
+    func test_presentInNavigationControllerAnimatedPresentFullScreen() {
+        viewController.presentInNavigationController(viewController: emptyViewController,
+                                                     animated: true,
+                                                     presentFullScreen: true)
+    }
+
+    func test_presentInNavigationControllerNotAnimated() {
+        viewController.presentInNavigationController(viewController: emptyViewController,
+                                                     animated: false,
+                                                     presentFullScreen: true)
+    }
+
+    func test_presentInNavigationControllerNotAnimatedPresentNotFullScreen() {
+        viewController.presentInNavigationController(viewController: emptyViewController,
+                                                     animated: false,
+                                                     presentFullScreen: false)
+    }
+
+    func test_presentInNavigationControllerAnimatedPresentNotFullScreen() {
+        viewController.presentInNavigationController(viewController: emptyViewController,
+                                                     animated: true,
+                                                     presentFullScreen: false)
+    }
+
+    func test_present() {
+        viewController.present(viewController: emptyViewController, animated: true, completion: nil)
+    }
+
+    func test_dismiss() {
+        viewController.dismiss(viewController: emptyViewController, animated: true, completion: nil)
+    }
+
+    func test_dismissInNavigationController() {
+        viewController.presentInNavigationController(viewController: emptyViewController,
+                                                     animated: true,
+                                                     presentFullScreen: true)
+        viewController.dismiss(viewController: emptyViewController, animated: true, completion: nil)
+    }
+
+    func test_embed() {
+        viewController.embed(viewController: emptyViewController)
     }
 
     func test_didCompleteOnboarding_callsDetachOnboardingAndRouteToMain() {
@@ -26,5 +77,72 @@ final class RootViewControllerTests: TestCase {
         viewController.didCompleteOnboarding()
 
         XCTAssertEqual(router.detachOnboardingAndRouteToMainCallCount, 1)
+    }
+
+    func test_messageWantsDismissal_callDismissViewController() {
+        XCTAssertEqual(router.detachMessageCallCount, 0)
+
+        viewController.messageWantsDismissal(shouldDismissViewController: true)
+
+        XCTAssertEqual(router.detachMessageCallCount, 1)
+    }
+
+    func test_callGGDWantsDismissal_callDismissViewController() {
+        XCTAssertEqual(router.detachCallGGDCallCount, 0)
+
+        viewController.callGGDWantsDismissal(shouldDismissViewController: true)
+
+        XCTAssertEqual(router.detachCallGGDCallCount, 1)
+    }
+
+    func test_didCompleteConsent() {
+        XCTAssertEqual(router.scheduleTasksCallCount, 0)
+
+        viewController.didCompleteConsent()
+
+        XCTAssertEqual(router.scheduleTasksCallCount, 1)
+    }
+
+    func test_endOfLifeRequestsRedirect() {
+        XCTAssertEqual(router.routeToWebviewCallCount, 0)
+
+        let mockUrl = URL(string: "http://someurl.com")
+
+        viewController.endOfLifeRequestsRedirect(to: mockUrl!)
+
+        XCTAssertEqual(router.routeToWebviewCallCount, 1)
+    }
+
+    func test_webviewRequestsDismissal() {
+        XCTAssertEqual(router.detachWebviewCallCount, 0)
+
+        viewController.webviewRequestsDismissal(shouldHideViewController: true)
+
+        XCTAssertEqual(router.detachWebviewCallCount, 1)
+    }
+
+    func test_developerMenuRequestsOnboardingFlow() {
+        XCTAssertEqual(router.routeToOnboardingCallCount, 0)
+
+        viewController.developerMenuRequestsOnboardingFlow()
+
+        XCTAssertEqual(router.routeToOnboardingCallCount, 1)
+    }
+
+    func test_developerMenuRequestUpdateOperatingSystem() {
+        XCTAssertEqual(router.routeToUpdateOperatingSystemCallCount, 0)
+
+        viewController.developerMenuRequestUpdateOperatingSystem()
+
+        XCTAssertEqual(router.routeToUpdateOperatingSystemCallCount, 1)
+    }
+
+    func test_developerMenuRequestUpdateApp() {
+        XCTAssertEqual(router.routeToUpdateAppCallCount, 0)
+
+        viewController.developerMenuRequestUpdateApp(appStoreURL: "mock",
+                                                     minimumVersionMessage: "mock")
+
+        XCTAssertEqual(router.routeToUpdateAppCallCount, 1)
     }
 }
