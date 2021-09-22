13be52483bce41d01a5f2d0dd638a44388928fc4
Roel Spruit <roelspruit@gmail.com>
2021-09-22 16:49:45 +0200

    Tests for storagecontroller

--- Sources/ENCoreTests/Common/Storage/StorageControllerTests.swift
+++ Sources/ENCoreTests/Common/Storage/StorageControllerTests.swift
@@ -12,20 +12,22 @@ import XCTest
 
 final class StorageControllerTests: TestCase {
 
-    private var fileManager: FileManagingMock!
-    private var localPathProvider: LocalPathProvidingMock!
-    private var environmentController: EnvironmentControllingMock!
-    private var storageController: StorageController!
+    private var sut: StorageController!
+    private var mockFileManager: FileManagingMock!
+    private var mockLocalPathProvider: LocalPathProvidingMock!
+    private var mockEnvironmentController: EnvironmentControllingMock!
+    private var mockDataAccessor: DataAccessingMock!
 
     override func setUp() {
         super.setUp()
 
-        fileManager = FileManagingMock()
-        localPathProvider = LocalPathProvidingMock()
-        environmentController = EnvironmentControllingMock()
+        mockFileManager = FileManagingMock()
+        mockLocalPathProvider = LocalPathProvidingMock()
+        mockEnvironmentController = EnvironmentControllingMock()
+        mockDataAccessor = DataAccessingMock()
 
-        localPathProvider.temporaryDirectoryUrl = URL(string: "/temp/")!
-        localPathProvider.pathHandler = { folder in
+        mockLocalPathProvider.temporaryDirectoryUrl = URL(string: "/temp/")!
+        mockLocalPathProvider.pathHandler = { folder in
             if folder == .documents {
                 return URL(string: "/documents/")!
             } else if folder == .cache {
@@ -34,17 +36,18 @@ final class StorageControllerTests: TestCase {
             return nil
         }
 
-        fileManager.urlsHandler = { folder, domainMask in
+        mockFileManager.urlsHandler = { folder, domainMask in
             return [URL(string: "/folder/folder\(folder.rawValue)")!]
         }
 
-        fileManager.contentsOfDirectoryHandler = { url, _, _ in
+        mockFileManager.contentsOfDirectoryHandler = { url, _, _ in
             return [url.appendingPathComponent("somefolder")]
         }
 
-        storageController = StorageController(fileManager: fileManager,
-                                              localPathProvider: localPathProvider,
-                                              environmentController: environmentController)
+        sut = StorageController(fileManager: mockFileManager,
+                                localPathProvider: mockLocalPathProvider,
+                                environmentController: mockEnvironmentController,
+                                dataAccessor: mockDataAccessor)
     }
 
     func test_prepareStore() {
@@ -52,52 +55,137 @@ final class StorageControllerTests: TestCase {
         let fileManagerExpectation = expectation(description: "filemanager")
         fileManagerExpectation.expectedFulfillmentCount = 2
 
-        XCTAssertEqual(fileManager.createDirectoryCallCount, 0)
-        fileManager.createDirectoryHandler = { _, _, _ in
+        XCTAssertEqual(mockFileManager.createDirectoryCallCount, 0)
+        mockFileManager.createDirectoryHandler = { _, _, _ in
             fileManagerExpectation.fulfill()
         }
 
-        storageController.prepareStore()
+        sut.prepareStore()
 
         waitForExpectations(timeout: 2, handler: nil)
 
-        XCTAssertEqual(fileManager.createDirectoryCallCount, 2)
-        XCTAssertEqual(fileManager.createDirectoryArgValues.first?.0.absoluteString, "/documents/store")
-        XCTAssertTrue(fileManager.createDirectoryArgValues.first?.1 == true)
-        XCTAssertEqual(fileManager.createDirectoryArgValues.last?.0.absoluteString, "/cache/store")
-        XCTAssertTrue(fileManager.createDirectoryArgValues.last?.1 == true)
+        XCTAssertEqual(mockFileManager.createDirectoryCallCount, 2)
+        XCTAssertEqual(mockFileManager.createDirectoryArgValues.first?.0.absoluteString, "/documents/store")
+        XCTAssertTrue(mockFileManager.createDirectoryArgValues.first?.1 == true)
+        XCTAssertEqual(mockFileManager.createDirectoryArgValues.last?.0.absoluteString, "/cache/store")
+        XCTAssertTrue(mockFileManager.createDirectoryArgValues.last?.1 == true)
     }
 
     func test_prepareStoreWhenAlreadyAvailable() {
 
-        XCTAssertEqual(fileManager.createDirectoryCallCount, 0)
+        XCTAssertEqual(mockFileManager.createDirectoryCallCount, 0)
 
-        storageController.prepareStore()
-        storageController.prepareStore()
+        sut.prepareStore()
+        sut.prepareStore()
 
-        XCTAssertEqual(fileManager.createDirectoryCallCount, 2)
+        XCTAssertEqual(mockFileManager.createDirectoryCallCount, 2)
     }
 
     func test_prepareStoreWithCreateDirectoryError() {
 
-        fileManager.createDirectoryHandler = { _, _, _ in
+        mockFileManager.createDirectoryHandler = { _, _, _ in
             throw (FileManaging.createDirectoryError)
         }
 
-        storageController.prepareStore()
+        sut.prepareStore()
 
-        XCTAssertEqual(storageController.storeAvailable, false)
+        XCTAssertEqual(sut.storeAvailable, false)
     }
 
     func test_prepareStore_shouldClearTemporaryDirectory() {
 
-        environmentController.isDebugVersion = false
-        XCTAssertEqual(fileManager.removeItemCallCount, 0)
+        mockEnvironmentController.isDebugVersion = false
+        XCTAssertEqual(mockFileManager.removeItemCallCount, 0)
 
-        storageController.prepareStore()
+        sut.prepareStore()
 
-        XCTAssertEqual(fileManager.removeItemCallCount, 1)
-        XCTAssertEqual(fileManager.removeItemArgValues[0].absoluteString, "/temp/")
+        XCTAssertEqual(mockFileManager.removeItemCallCount, 1)
+        XCTAssertEqual(mockFileManager.removeItemArgValues[0].absoluteString, "/temp/")
+    }
+
+    func test_store() throws {
+        // Arrange
+        sut.prepareStore()
+
+        let storeCompletionExpectation = expectation(description: "storeCompletionExpectation")
+        let testStorageKey = StorageKey(name: "test", storeType: .insecure(volatile: false, maximumAge: 20))
+        let data = "some string".data(using: .utf8)!
+
+        // Act
+        sut.store(data: data, identifiedBy: testStorageKey) { error in
+            XCTAssertNil(error)
+            storeCompletionExpectation.fulfill()
+        }
+
+        // Assert
+        waitForExpectations()
+        XCTAssertEqual(mockDataAccessor.writeCallCount, 1)
+        XCTAssertEqual(mockDataAccessor.writeArgValues.first?.0, data)
+        XCTAssertEqual(mockDataAccessor.writeArgValues.first?.1.absoluteString, "/documents/store/test")
+    }
+
+    func test_retrieve() throws {
+        // Arrange
+        sut.prepareStore()
+        mockFileManager.fileExistsHandler = { _, _ in return true }
+        mockDataAccessor.readHandler = { url in return "some string".data(using: .utf8) }
+        let testStorageKey = StorageKey(name: "test", storeType: .insecure(volatile: false, maximumAge: 20))
+
+        // Act
+        let retrievedData = try XCTUnwrap(sut.retrieveData(identifiedBy: testStorageKey))
+
+        // Assert
+        XCTAssertEqual(mockDataAccessor.readCallCount, 1)
+        XCTAssertEqual(mockDataAccessor.readArgValues.first?.absoluteString, "/documents/store/test")
+        XCTAssertEqual(mockFileManager.fileExistsCallCount, 1)
+        XCTAssertEqual(mockFileManager.fileExistsArgValues.first?.0, "/documents/store/test")
+
+        let retrievedString = String(data: retrievedData, encoding: .utf8)
+        XCTAssertEqual(retrievedString, "some string")
+    }
+
+    func test_retrieve_passedMaximumAge() throws {
+        // Arrange
+        let date = Date(timeIntervalSince1970: 1593538088) // 30/06/20 17:28
+        DateTimeTestingOverrides.overriddenCurrentDate = date
+
+        sut.prepareStore()
+        mockFileManager.attributesOfItemHandler = { path in [.modificationDate: date.addingTimeInterval(-1000)] }
+        let testStorageKey = StorageKey(name: "test", storeType: .insecure(volatile: false, maximumAge: 20))
+
+        // Act
+        let retrievedData = sut.retrieveData(identifiedBy: testStorageKey)
+
+        // Assert
+        XCTAssertNil(retrievedData)
+        XCTAssertEqual(mockFileManager.attributesOfItemCallCount, 1)
+        XCTAssertEqual(mockFileManager.attributesOfItemArgValues.first, "/documents/store/test")
+    }
+
+    func test_removeData() {
+        // Arrange
+        sut.prepareStore()
+        let completionExpectation = expectation(description: "completionExpectation")
+        let testStorageKey = StorageKey(name: "test", storeType: .insecure(volatile: false, maximumAge: 20))
+
+        // Mock existing data
+        mockFileManager.fileExistsHandler = { _, _ in return true }
+        mockDataAccessor.readHandler = { url in return "some string".data(using: .utf8) }
+
+        XCTAssertEqual(mockFileManager.removeItemCallCount, 1)
+
+        // Act
+        sut.removeData(for: testStorageKey) { error in
+            XCTAssertNil(error)
+            completionExpectation.fulfill()
+        }
+
+        // Assert
+        waitForExpectations()
+
+        XCTAssertEqual(mockFileManager.removeItemCallCount, 2)
+        XCTAssertEqual(mockFileManager.removeItemArgValues.first?.absoluteString, "/temp/")
+        XCTAssertEqual(mockFileManager.removeItemArgValues.last?.absoluteString, "/documents/store/test")
     }
 
     private enum FileManaging: Error {
