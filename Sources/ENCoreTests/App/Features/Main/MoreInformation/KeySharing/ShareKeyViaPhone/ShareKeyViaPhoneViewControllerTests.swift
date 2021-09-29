c90d94146bc673b943e7762fdbf01e957fab8a5d
Roel Spruit <roelspruit@gmail.com>
2021-09-29 17:03:02 +0200

    Fix flaky tests

--- Sources/ENCoreTests/App/Features/Main/MoreInformation/KeySharing/ShareKeyViaPhone/ShareKeyViaPhoneViewControllerTests.swift
+++ Sources/ENCoreTests/App/Features/Main/MoreInformation/KeySharing/ShareKeyViaPhone/ShareKeyViaPhoneViewControllerTests.swift
@@ -31,10 +31,10 @@ final class ShareKeyViaPhoneViewControllerTests: TestCase {
         mockInterfaceOrientationStream.isLandscape = BehaviorSubject(value: false)
 
         viewController = ShareKeyViaPhoneViewController(theme: theme,
-                                                exposureController: mockExposureController,
-                                                exposureStateStream: mockExposureStateStream,
-                                                interfaceOrientationStream: mockInterfaceOrientationStream,
-                                                withBackButton: false)
+                                                        exposureController: mockExposureController,
+                                                        exposureStateStream: mockExposureStateStream,
+                                                        interfaceOrientationStream: mockInterfaceOrientationStream,
+                                                        withBackButton: false)
         viewController.router = mockRouter
 
         // force viewDidLoad
@@ -42,38 +42,60 @@ final class ShareKeyViaPhoneViewControllerTests: TestCase {
     }
 
     func test_inactiveState_callsRouterToShowCard() {
+        let completionExpectation = expectation(description: "completionExpectation")
+        mockRouter.showInactiveCardHandler = { _ in
+            completionExpectation.fulfill()
+        }
         XCTAssertEqual(mockRouter.showInactiveCardCallCount, 0)
 
         exposureStateSubject.onNext(.init(notifiedState: .notNotified,
                                           activeState: .authorizationDenied))
 
+        waitForExpectations()
         XCTAssertEqual(mockRouter.showInactiveCardCallCount, 1)
     }
 
     func test_activeState_callsRouterToRemoveAnyCard() {
+        let completionExpectation = expectation(description: "completionExpectation")
+        mockRouter.removeInactiveCardHandler = {
+            completionExpectation.fulfill()
+        }
         XCTAssertEqual(mockRouter.removeInactiveCardCallCount, 0)
 
         exposureStateSubject.onNext(.init(notifiedState: .notNotified,
                                           activeState: .active))
 
+        waitForExpectations()
         XCTAssertEqual(mockRouter.removeInactiveCardCallCount, 1)
     }
 
     func test_activeState_requestsLabConfirmationKey() {
+        let completionExpectation = expectation(description: "completionExpectation")
+        mockExposureController.requestLabConfirmationKeyHandler = { _ in
+            completionExpectation.fulfill()
+        }
         XCTAssertEqual(mockExposureController.requestLabConfirmationKeyCallCount, 0)
 
         exposureStateSubject.onNext(.init(notifiedState: .notNotified,
                                           activeState: .active))
 
+        waitForExpectations()
         XCTAssertEqual(mockExposureController.requestLabConfirmationKeyCallCount, 1)
     }
 
     func test_inactiveState_doesNotRequestLabConfirmationKey() {
+        let completionExpectation = expectation(description: "completionExpectation")
+        completionExpectation.isInverted = true
+        mockExposureController.requestLabConfirmationKeyHandler = { _ in
+            completionExpectation.fulfill()
+        }
+
         XCTAssertEqual(mockExposureController.requestLabConfirmationKeyCallCount, 0)
 
         exposureStateSubject.onNext(.init(notifiedState: .notNotified,
                                           activeState: .authorizationDenied))
 
+        waitForExpectations()
         XCTAssertEqual(mockExposureController.requestLabConfirmationKeyCallCount, 0)
     }
 }
