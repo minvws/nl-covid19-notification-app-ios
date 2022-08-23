be9379f278c7e87f6dc6f159e89164ac605f2d44
Thom Hoekstra <thom@egeniq.com>
2022-08-23 12:18:00 +0200

    Update endpoints, signing and pinning

--- Sources/ENCoreTests/Common/NetworkManager/Config/NetworkConfigurationTests.swift
+++ Sources/ENCoreTests/Common/NetworkManager/Config/NetworkConfigurationTests.swift
@@ -18,37 +18,37 @@ class NetworkConfigurationTests: TestCase {
 
     func test_manifestUrl() {
         XCTAssertEqual(sut.manifestUrl?.absoluteString,
-                       "https://test.coronamelder-dist.nl/\(sut.cdn.path)/manifest")
+                       "https://dist.test.coronamelder.nl/\(sut.cdn.path)/manifest")
     }
 
     func test_exposureKeySetUrl() {
         XCTAssertEqual(sut.exposureKeySetUrl(identifier: "identifier")?.absoluteString,
-                       "https://test.coronamelder-dist.nl/\(sut.cdn.path)/exposurekeyset/identifier")
+                       "https://dist.test.coronamelder.nl/\(sut.cdn.path)/exposurekeyset/identifier")
     }
 
     func test_riskCalculationParametersUrl() {
         XCTAssertEqual(sut.riskCalculationParametersUrl(identifier: "identifier")?.absoluteString,
-                       "https://test.coronamelder-dist.nl/\(sut.cdn.path)/riskcalculationparameters/identifier")
+                       "https://dist.test.coronamelder.nl/\(sut.cdn.path)/riskcalculationparameters/identifier")
     }
 
     func test_appConfigUrl() {
         XCTAssertEqual(sut.appConfigUrl(identifier: "identifier")?.absoluteString,
-                       "https://test.coronamelder-dist.nl/\(sut.cdn.path)/appconfig/identifier")
+                       "https://dist.test.coronamelder.nl/\(sut.cdn.path)/appconfig/identifier")
     }
 
     func test_treatmentPerspectiveUrl() {
-        XCTAssertEqual(sut.treatmentPerspectiveUrl(identifier: "identifier")?.absoluteString, "https://test.coronamelder-dist.nl/\(sut.cdn.path)/resourcebundle/identifier")
+        XCTAssertEqual(sut.treatmentPerspectiveUrl(identifier: "identifier")?.absoluteString, "https://dist.test.coronamelder.nl/\(sut.cdn.path)/resourcebundle/identifier")
     }
 
     func test_registerUrl() {
-        XCTAssertEqual(sut.registerUrl?.absoluteString, "https://test.coronamelder-api.nl/v2/register")
+        XCTAssertEqual(sut.registerUrl?.absoluteString, "https://api.test.coronamelder.nl/v2/register")
     }
 
     func test_postKeysUrl() {
-        XCTAssertEqual(sut.postKeysUrl(signature: "signature")?.absoluteString, "https://test.coronamelder-api.nl/\(sut.api.path)/postkeys?sig=signature")
+        XCTAssertEqual(sut.postKeysUrl(signature: "signature")?.absoluteString, "https://api.test.coronamelder.nl/\(sut.api.path)/postkeys?sig=signature")
     }
 
     func test_stopKeysUrl() {
-        XCTAssertEqual(sut.stopKeysUrl(signature: "signature")?.absoluteString, "https://test.coronamelder-api.nl/\(sut.api.path)/stopkeys?sig=signature")
+        XCTAssertEqual(sut.stopKeysUrl(signature: "signature")?.absoluteString, "https://api.test.coronamelder.nl/\(sut.api.path)/stopkeys?sig=signature")
     }
 }
